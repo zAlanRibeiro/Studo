059cfc8ec0fb32df7c6181a0c67e98829a875645
Camilly Vitória <vitoriacamilly308@gmail.com>
2026-04-14 22:45:34 -0300

    chore(app): remove arquivos padrões do template Expo

--- app/modal.tsx
+++ app/modal.tsx
@@ -1,29 +1,9 @@
-import { Link } from 'expo-router';
-import { StyleSheet } from 'react-native';
+import { Text, View } from 'react-native';
 
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-
-export default function ModalScreen() {
+export default function Modal() {
   return (
-    <ThemedView style={styles.container}>
-      <ThemedText type="title">This is a modal</ThemedText>
-      <Link href="/" dismissTo style={styles.link}>
-        <ThemedText type="link">Go to home screen</ThemedText>
-      </Link>
-    </ThemedView>
+    <View>
+      <Text>Meu modal</Text>
+    </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
-});
